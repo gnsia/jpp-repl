33932356d092f5283be5c7e22ee57bf4e4599c43
hand-stencil <64590931+gnsia@users.noreply.github.com>
2022-09-14 07:35:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const liveAlbumSrc = [
+  'https://i.ibb.co/mDBfCrL/demo13.png',
+  'https://i.ibb.co/1LhSxM0/demo12.png',
+  'https://i.ibb.co/Ykrrx4y/demo11.png',
+  'https://i.ibb.co/DgMdLrt/demo10.png',
+  'https://i.ibb.co/stS0PM6/demo09.png',
+  'https://i.ibb.co/gDw0WjN/demo07.png',
+  'https://i.ibb.co/6XchcN6/demo08.png',
+];
+
+const portraitAlbumSrc = [
+  'https://i.ibb.co/4TGJ9RW/demo06.png',
+  'https://i.ibb.co/XWm8Pjb/demo05.png',
+  'https://i.ibb.co/yXpLdvv/demo04.png',
+  'https://i.ibb.co/kcRXcrB/demo03.png',
+  'https://i.ibb.co/L5KFr7M/demo01.png',
+  'https://i.ibb.co/Q9VwTNW/demo02.png',
+];
